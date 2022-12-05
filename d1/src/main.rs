3caee034150993f21661018b53fe9b4b16a12827
matthiasdebernardini <m.f.debern@protonmail.com>
2022-12-05 09:57:49 -0600

    adding d1

--- d1/src/main.rs
+++ d1/src/main.rs
@@ -1,25 +1,18 @@
 #[inpt::main]
 fn main(i: String) {
-    let mut elves: Vec<Vec<i32>> = vec![];
     let mut v: Vec<i32> = vec![];
+    let mut max = 0;
     for l in i.lines() {
         match l.parse::<i32>() {
             Ok(n) => v.push(n),
             _ => {
-                elves.push(v.clone());
+                let calories = v.iter().sum();
+                if calories > max {
+                    max = calories;
+                }
                 v.clear()
             }
         }
     }
-    let mut max = 0;
-    let mut best_elf = 0;
-    for (i, e) in elves.iter().enumerate() {
-        let calories:i32 = e.iter().sum();
-        if calories > max {
-            max = calories;
-            best_elf = i;
-        }
-    }
-    
-    println!("{best_elf:?} {max}")
+    println!("{max}")
 }
